a23fc6726cb274f5172a39bf5bf297c4e9e2458c
Pedro Ribeiro <pedro.ribeiro@smartmed.world>
2018-12-09 17:14:59 +0000

    Initial Commit

---
+++ BuildTest/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BuildTest
+//
+//  Created by Pedro Ribeiro on 12/9/18.
+//  Copyright © 2018 SmartMed. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
